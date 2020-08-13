f3686e0f30edf78e0ae443dad557d1479da1d73a
allecramarcella <allecramarcella.dev@gmail.com>
2020-08-13 15:49:30 +0200

    new design

--- script2.js
+++ script2.js
@@ -1,52 +1,52 @@
 // images Ironhackers
 const marcella = new Image()
-marcella.src = './images/Marcella2.png'
+marcella.src = './images/marcella.png'
 
 const yuka = new Image()
-yuka.src = './images/Yuka.png'
+yuka.src = './images/yuka.png'
 
 const lisette = new Image()
-lisette.src = './images/Lisette2.png'
+lisette.src = './images/lisette.png'
 
 const anna = new Image()
-anna.src = './images/Anna.png'
+anna.src = './images/anna.png'
 
 const mahmut = new Image()
-mahmut.src = './images/Mahmut.png'
+mahmut.src = './images/mahmut.png'
 
 const kenouly = new Image()
-kenouly.src = './images/Kenouly.png'
+kenouly.src = './images/kenouly.png'
 
 const ironhackersImgArray = [marcella, yuka, lisette, anna, mahmut, kenouly]
 
 // images teachers
 const jorg = new Image()
-jorg.src = './images/Jorg.png'
+jorg.src = './images/jorg.png'
 
 const guido = new Image()
-guido.src = './images/Guido.png'
+guido.src = './images/guido.png'
 
 const teachersImgArray = [guido, jorg]
 
 
 // clicked Images ironhackers
 const mahmutClicked = new Image()
-mahmutClicked.src = './images/Mahmut clicked.png'
+mahmutClicked.src = './images/mahmutclicked.png'
 
 const annaClicked = new Image()
-annaClicked.src = './images/anna clicked.png'
+annaClicked.src = './images/annaclicked.png'
 
 const kenoulyClicked = new Image()
-kenoulyClicked.src = './images/kenouly clicked.png'
+kenoulyClicked.src = './images/kenoulyclicked.png'
 
 const marcellaClicked = new Image()
-marcellaClicked.src = './images/marcella clicked.png'
+marcellaClicked.src = './images/marcellaclicked.png'
 
 const lisetteClicked = new Image()
-lisetteClicked.src = './images/Lisette clicked.png'
+lisetteClicked.src = './images/lisetteclicked.png'
 
 const yukaClicked = new Image()
-yukaClicked.src = './images/yuka clicked.png'
+yukaClicked.src = './images/yukaclicked.png'
 
 // clicked Images teachers
 const jorgClicked = new Image()
@@ -61,11 +61,6 @@ jorgIntro.src = './images/introductionJorg.png'
 
 const guidoIntro = new Image()
 guidoIntro.src =  './images/introductionGuido.png'
-
-
-// //background img
-// const backgroundImg = new Image()
-// backgroundImg.src = './images/background.jpg'
 
 
 const canvas = document.getElementById('canvas')
@@ -91,34 +86,26 @@ class GameBoard {
     }
 
     startScreen(){
-        let startScreen = document.createElement('div')
+        const startScreen = document.createElement('div')
         startScreen.setAttribute('id', 'start-screen')
 
-        let divBefore = document.getElementsByClassName('game-border-top')[0]
-        let parentContainer = document.getElementById('container')
+        const divBefore = document.getElementsByClassName('game-border-top')[0]
+        const parentContainer = document.getElementById('container')
         parentContainer.insertBefore(startScreen, divBefore)
 
-        let nav = document.createElement('nav')
-        nav.setAttribute('id', 'nav-bar')
-        let parentStartScreen = document.getElementById('start-screen')
-        parentStartScreen.appendChild(nav)
-        let navTitle = document.createElement('p')
-        navTitle.setAttribute('id', 'nav-title')
-        navTitle.innerHTML = 'Hit me, Spaghetti, one more time...'
-        let parentNav = document.getElementById('nav-bar')
-        parentNav.appendChild(navTitle)
 
-        let middlepart = document.createElement('div')
+        const parentStartScreen = document.getElementById('start-screen')
+        const middlepart = document.createElement('div')
         parentStartScreen.appendChild(middlepart)
         middlepart.setAttribute('id', 'middle-part')
-        let titelGame = document.createElement('h1')
+        const titelGame = document.createElement('h1')
         titelGame.innerHTML = 'Hit me, Spaghetti, one more time...'
-        let parentMiddlePart= document.getElementById('middle-part')
+        const parentMiddlePart= document.getElementById('middle-part')
         parentMiddlePart.appendChild(titelGame)
 
-        let explanationText1 = document.createElement('p')
-        let explanationText2 = document.createElement('p')
-        let explanationText3 = document.createElement('h3')
+        const explanationText1 = document.createElement('p')
+        const explanationText2 = document.createElement('p')
+        const explanationText3 = document.createElement('h3')
         explanationText1.innerHTML = 'In a Breakout Room far far away......six brave students gave up their summer.....and autumn.....to become the next best coding ninjas.' 
         explanationText2.innerHTML = 'Since teaching is like throwing spaghetti and hoping some will stick (read: quote teacher)......all you have to do is to throw as much spaghetti at the students as you can. But BE AWARE.....once in a while the teacher and teacher assistant join the Breakout Room......and as you might guess......hitting them will get you into trouble!!'
         explanationText3.innerHTML = '[You can throw spaghetti by clicking with your mouse]'
@@ -127,7 +114,7 @@ class GameBoard {
         parentMiddlePart.appendChild(explanationText2)
         parentMiddlePart.appendChild(explanationText3)
 
-        let button = document.createElement('button')
+        const button = document.createElement('button')
         button.innerHTML = 'Join Breakout Room'
         parentMiddlePart.appendChild(button)
 
@@ -247,13 +234,13 @@ class Counter {
     }
 
     drawLivesCounter(ctx) {
-        ctx.fillStyle = 'white'
+        ctx.fillStyle = '#f1a725'
         ctx.font = '22px Slackey'
         ctx.fillText(`Lives: ${this.lives}`, 30, 50)
     }
 
     drawLevelCounter(ctx){
-        ctx.fillStyle = 'white'
+        ctx.fillStyle = '#f1a725'
         ctx.font = '22px Slackey'
         ctx.fillText(`Level: ${this.levels}`, window.innerWidth - 120, 50)
     }
@@ -336,45 +323,43 @@ class Game {
   
     
     addStudent(){
-        this.shuffleIronhackersImgArray(ironhackersImgArray)
+        if(this.addTeachers(true)) {
+            this.addTeachers(called)
+        } else {
+            this.shuffleIronhackersImgArray(ironhackersImgArray)
         
-        for(let i= 0; i < this.shuffledIronhackersImgArr.length; i++) {
-            if(!this.randomIronhackers.includes(this.shuffledIronhackersImgArr[i]) && this.randomIronhackers.length < this.shuffledIronhackersImgArr.length) {
-                this.randomIronhackers.push(this.shuffledIronhackersImgArr[i])
-                this.randomImg = this.shuffledIronhackersImgArr[i]
-                break
-            } else if(this.randomIronhackers.includes(this.shuffledIronhackersImgArr[i]) && this.randomIronhackers.length < this.shuffledIronhackersImgArr.length) {
-                continue
-            } else if(this.randomIronhackers.length === this.shuffledIronhackersImgArr.length) {
-                this.randomIronhackers.splice(0,this.randomIronhackers.length)
+            for(let i= 0; i < this.shuffledIronhackersImgArr.length; i++) {
+                if(!this.randomIronhackers.includes(this.shuffledIronhackersImgArr[i]) && this.randomIronhackers.length < this.shuffledIronhackersImgArr.length) {
+                    this.randomIronhackers.push(this.shuffledIronhackersImgArr[i])
+                    this.randomImg = this.shuffledIronhackersImgArr[i]
+                    break
+                } else if(this.randomIronhackers.includes(this.shuffledIronhackersImgArr[i]) && this.randomIronhackers.length < this.shuffledIronhackersImgArr.length) {
+                    continue
+                } else if(this.randomIronhackers.length === this.shuffledIronhackersImgArr.length) {
+                    this.randomIronhackers.splice(0,this.randomIronhackers.length)
+                }
             }
-        }
     
-  
-        this.randomX =  30 + (Math.random() * (window.innerWidth - 310))
+            this.randomX =  30 + (Math.random() * (window.innerWidth - 310))
+            this.newIronhacker = new Ironhacker(this.randomImg, true, this.randomX, 0, this.randomSpeed, 250, 141)
 
-        this.newIronhacker = new Ironhacker(this.randomImg, true, this.randomX, 0, this.randomSpeed, 250, 141)
-
-        this.ironhackers.push(this.newIronhacker)  
-        this.ironhackersClicked.push(this.newIronhacker)
-        this.ironhackersSpeedUp.push(this.newIronhacker)
-        this.addTeachers()
+            this.ironhackers.push(this.newIronhacker)  
+        }
+        
     }
 
-    addTeachers(){
+    addTeachers(called){
+        this.called = called
         const randomNumber = 0 + Math.floor(Math.random() * 7)
         if(randomNumber === 1) {
             this.shuffleIronhackersImgArray(teachersImgArray)
             this.randomImg = this.shuffledIronhackersImgArr[0]
 
-            // this.randomSpeed = 2 + (Math.random() * 4)
             this.randomX =  230 + (Math.random() * (window.innerWidth - 680))
- 
             this.newIronhacker = new Ironhacker(this.randomImg, false, this.randomX, 0, this.randomSpeed, 250, 141)
-  
-            this.ironhackers.push(this.newIronhacker)        
-            this.ironhackersClicked.push(this.newIronhacker)
-            this.ironhackersSpeedUp.push(this.newIronhacker)
+            this.ironhackers.push(this.newIronhacker)     
+
+            return  this.called === true   
         } 
     }
 
@@ -438,13 +423,12 @@ class Game {
         } 
     }
 
-    // gameOver(ctx){
-    //     let soundGameOver = new AddSound('./Sounds/gemeover.swf.mp3')
-    //     soundGameOver.play()
+    gameOver(ctx){
+        const soundGameOver = new AddSound('./Sounds/gemeover.swf.mp3')
+        soundGameOver.play()
 
-    //     ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
-    //     this.updateLeves(ctx)
-    //     // this.updateLives(ctx)
+        ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
+        this.updateCounters(ctx)
     //     ctx.font = '8vw Slackey'
     
     //     let dashLen = 220
@@ -479,7 +463,46 @@ class Game {
     //             if (i < txt.length) requestAnimationFrame(loop)
     //         }
     //         })();
-    // }
+        // const gameOverScreen = document.createElement('div')
+        // gameOverScreen.setAttribute('id', 'gameOver-screen')
+
+        // const divBefore = document.getElementsByClassName('game-border-top')[0]
+        // const parentContainer = document.getElementById('container')
+        // parentContainer.insertBefore(gameOverScreen, divBefore)
+
+        // const title = document.createElement('h2')
+        // // let explanationText2 = document.createElement('p')
+        // // let explanationText3 = document.createElement('h3')
+        // title.innerHTML = 'Game Over' 
+        // // explanationText2.innerHTML = 'Since teaching is like throwing spaghetti and hoping some will stick (read: quote teacher)......all you have to do is to throw as much spaghetti at the students as you can. But BE AWARE.....once in a while the teacher and teacher assistant join the Breakout Room......and as you might guess......hitting them will get you into trouble!!'
+        // // explanationText3.innerHTML = '[You can throw spaghetti by clicking with your mouse]'
+    
+        // gameOverScreen.appendChild(title)
+        // // parentMiddlePart.appendChild(explanationText2)
+        // // parentMiddlePart.appendChild(explanationText3)
+
+        // const textWrapper = document.getElementById('gameOver-screen');
+        // textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+        // anime.timeline({loop: true})
+        // .add({
+        //     targets: '#gameOver-screen .letter',
+        //     scale: [4,1],
+        //     opacity: [0,1],
+        //     translateZ: 0,
+        //     easing: "easeOutExpo",
+        //     duration: 950,
+        //     delay: (el, i) => 70*i
+        // }).add({
+        //     targets: '#gameOver-screen',
+        //     opacity: 0,
+        //     duration: 1000,
+        //     easing: "easeOutExpo",
+        //     delay: 1000
+        // });
+        
+
+    }
     
     // winner(ctx){
     //     ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
